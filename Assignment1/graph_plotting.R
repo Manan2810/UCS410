a7ecbc89c2f6de647bb63a483f2233f077a0b954
Manan Mehra <mananmehra330@gmail.com>
2023-08-17 16:08:51 +0530

    graph plotting code added

---
+++ Assignment1/graph_plotting.R
@@ -0,0 +1,22 @@
+# number of elements i x should be equal to number of elements in y
+
+#simple points plotted
+x=c(2,3,4,6)
+y=c(3,5,6,8)
+plot(x,y,pch=5,type="l")
+#here type="l" is representing lines that join the plotted points
+
+#pie chart
+x=c(2,3,4,6)
+y=c(3,5,6,8)
+pie(x,y)
+
+#bar graph
+x=c(2,3,4,6)
+y=c(3,5,6,8)
+barplot(x,y,col=rgb(1,0.8,0.4))
+
+a=seq(1,5,by=1)
+b=seq(6,10,by=1)
+plot(a,b,pch=5,type="l")
+
